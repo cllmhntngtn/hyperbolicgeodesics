d3c4cc4949eaca02f7e0934dd9685d8715d6ef11
cllmhntngtn <88731855+cllmhntngtn@users.noreply.github.com>
2021-08-11 23:34:28 +0100

    Update upperhalfplane.js

--- upperhalfplane.js
+++ upperhalfplane.js
@@ -1,4 +1,4 @@
-function windowResized() {
+unction windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
@@ -55,8 +55,11 @@ function setup() {
   buttonAddAnotherPair.style('background-color', 'whitesmoke');
 }
 
-var x = [];
-var y = [];
+var x = ['',''];
+var y = ['',''];
+
+var permX = [];
+var permY = [];
 
 vertexLabels = 'abcdefghijklmnopqrstuvwxyz';
 firstVertexLabelNumber = 0;
@@ -64,25 +67,27 @@ secondVertexLabelNumber = 1;
 
 function reA() {
   var realA = inputReA.value();
-  x.push(realA);
-}
-
-function reB() {
-  var realB = inputReB.value();
-  x.push(realB);
+  x[0] = realA;
 }
 
 function imA() {
   var imagA = inputImA.value();
-  y.push(imagA);
+  y[0] = imagA;
+}
+
+function reB() {
+  var realB = inputReB.value();
+  x[1] = realB;
 }
 
 function imB() {
   var imagB = inputImB.value();
-  y.push(imagB);
+  y[1] = imagB;
 }
 
 function addAnotherPair() {
+  permX.push(x[0]); permX.push(x[1]);
+  permY.push(y[0]); permY.push(y[1]);
   x.pop(); x.pop();
   y.pop(); y.pop();
   inputReA.value('');
@@ -94,6 +99,8 @@ function addAnotherPair() {
 }
 
 function draw() {
+  background('white');
+
   push();
   noStroke();
   fill('white');
@@ -152,22 +159,24 @@ function draw() {
     }
   pop();
 
-  // calculating the relevant semi-circle
-  var circleCentre = (x[0] ** 2 + y[0] ** 2 - x[1] ** 2 - y[1] ** 2) / (2 * x[0] - 2 * x[1]);
-  var circleRadius = dist(x[0], y[0], circleCentre, 0);
-  // calculating angles for the arc along the semi-circle
-  var alpha = acos((- circleRadius * (x[0] - circleCentre)) / (circleRadius * sqrt((x[0] - circleCentre) ** 2 + y[0] ** 2)));
-  var beta = acos((- circleRadius * (x[1] - circleCentre)) / (circleRadius * sqrt((x[1] - circleCentre) ** 2 + y[1] ** 2)));
+  if (x[0] && y[0] && x[1] && y[1]) {
+    // calculating the relevant semi-circle
+    var circleCentre = (x[0] ** 2 + y[0] ** 2 - x[1] ** 2 - y[1] ** 2) / (2 * x[0] - 2 * x[1]);
+    var circleRadius = dist(x[0], y[0], circleCentre, 0);
+    // calculating angles for the arc along the semi-circle
+    var alpha = acos((- circleRadius * (x[0] - circleCentre)) / (circleRadius * sqrt((x[0] - circleCentre) ** 2 + y[0] ** 2)));
+    var beta = acos((- circleRadius * (x[1] - circleCentre)) / (circleRadius * sqrt((x[1] - circleCentre) ** 2 + y[1] ** 2)));
+  }
 
   // drawing the geodesics
   push();
   strokeWeight(6);
   strokeCap(SQUARE);
   noFill();
-  if (x[0] === x[1] && y.length === 2) {
+  if (x[0] === x[1] && x[0] && y[0] && x[1] && y[1]) {
     stroke('darkgray');
     line(xOrigin + x[0] * xStep, yOrigin,
-      xOrigin + x[1] * xStep, yMax);
+      xOrigin + x[1] * xStep, 0);
     stroke('dodgerblue');
     line(xOrigin + x[0] * xStep, yOrigin - y[0] * yStep,
       xOrigin + x[1] * xStep, yOrigin - y[1] * yStep);
@@ -192,34 +201,43 @@ function draw() {
 
   // drawing the points in the half-plane
   push();
-  strokeWeight(2);
-  fill('white');
-  circle(xOrigin + x[0] * xStep, yOrigin - y[0] * yStep, 25);
-  circle(xOrigin + x[1] * xStep, yOrigin - y[1] * yStep, 25);
-  fill('black');
-  textFont('Helvetica');
-  textSize(20);
-  text(vertexLabels[firstVertexLabelNumber], xOrigin + x[0] * xStep, yOrigin - y[0] * yStep);
-  text(vertexLabels[secondVertexLabelNumber], xOrigin + x[1] * xStep, yOrigin - y[1] * yStep);
+  if (x[0] && y[0]) {
+    strokeWeight(2);
+    fill('white');
+    circle(xOrigin + x[0] * xStep, yOrigin - y[0] * yStep, 25);
+    fill('black');
+    textFont('Helvetica');
+    textSize(20);
+    text(vertexLabels[firstVertexLabelNumber], xOrigin + x[0] * xStep, yOrigin - y[0] * yStep);
+  }
+  if (x[1] && y[1]) {
+    strokeWeight(2);
+    fill('white');
+    circle(xOrigin + x[1] * xStep, yOrigin - y[1] * yStep, 25);
+    fill('black');
+    textFont('Helvetica');
+    textSize(20);
+    text(vertexLabels[secondVertexLabelNumber], xOrigin + x[1] * xStep, yOrigin - y[1] * yStep);
+  }
   pop();
 
   // whiteout of the background outside the plane
-  push();
-  fill('white');
-  noStroke();
-  beginShape();
-    vertex(0, 0);
-    vertex(windowWidth, 0);
-    vertex(windowWidth, windowHeight);
-    vertex(0, windowHeight);
-    beginContour();
-      vertex(xMin - 2, yMin + 22);
-      vertex(xMax + 2, yMin + 22);
-      vertex(xMax + 2, yMax - 2);
-      vertex(xMin - 2, yMax - 2);
-    endContour();
-  endShape();
-  pop();
+  //push();
+  //fill('white');
+  //noStroke();
+  //beginShape();
+  //  vertex(0, 0);
+  //  vertex(windowWidth, 0);
+  //  vertex(windowWidth, windowHeight);
+  //  vertex(0, windowHeight);
+  //  beginContour();
+  //    vertex(xMin - 2, yMin + 22);
+  //    vertex(xMax + 2, yMin + 22);
+  //    vertex(xMax + 2, yMax - 2);
+  //    vertex(xMin - 2, yMax - 2);
+  //  endContour();
+  //endShape();
+  //pop();
 
   // input labels
   push();
@@ -238,18 +256,87 @@ function draw() {
   text('Im', xOrigin, yMax - 25);
   pop();
 
-  // naming the inputted points - need to find a way to keep them when adding another pair
-  //push();
-  //fill('black');
-  //textFont('Helvetica');
-  //textSize(20);
-  //if (x[0] && y[0]) {
-  //  text(vertexLabels[firstVertexLabelNumber] + ' = ' + x[0] + ' + ' + y[0] + 'i',
-  //        5 * windowWidth / 6, (firstVertexLabelNumber + 1.5) * windowHeight / 20 + 2);
-  //}
-  //if (x[1] && y[1]) {
-  //  text(vertexLabels[secondVertexLabelNumber] + ' = ' + x[1] + ' + ' + y[1] + 'i',
-  //        5 * windowWidth / 6, (secondVertexLabelNumber + 1.5) * windowHeight / 20 + 2);
-  //}
-  //pop();
+
+  // permanent drawings for after pressing the addAnotherPair button
+
+  for (let index = 0; index < permX.length; index += 2) {
+      if (permX[index] && permY[index] && permX[index + 1] && permY[index + 1]) {
+        // calculating the relevant semi-circle
+        var permCircleCentre = (permX[index] ** 2 + permY[index] ** 2 - permX[index + 1] ** 2 - permY[index + 1] ** 2) / (2 * permX[index] - 2 * permX[index + 1]);
+        var permCircleRadius = dist(permX[index], permY[index], permCircleCentre, 0);
+        // calculating angles for the arc along the semi-circle
+        var permAlpha = acos((- permCircleRadius * (permX[index] - permCircleCentre)) / (permCircleRadius * sqrt((permX[index] - permCircleCentre) ** 2 + permY[index] ** 2)));
+        var permBeta = acos((- permCircleRadius * (permX[index + 1] - permCircleCentre)) / (permCircleRadius * sqrt((permX[index + 1] - permCircleCentre) ** 2 + permY[index + 1] ** 2)));
+      }
+
+      // drawing the geodesics
+      push();
+      strokeWeight(6);
+      strokeCap(SQUARE);
+      noFill();
+      if (permX[index] === permX[index + 1] && permX[index] && permX[index + 1] && permY[index] && permY[index + 1]) {
+        stroke('darkgray');
+        line(xOrigin + permX[index] * xStep, yOrigin,
+          xOrigin + permX[index + 1] * xStep, 0);
+        stroke('dodgerblue');
+        line(xOrigin + permX[index] * xStep, yOrigin - permY[index] * yStep,
+          xOrigin + permX[index + 1] * xStep, yOrigin - permY[index + 1] * yStep);
+      } else if (permX[index] < permX[index + 1]) {
+        stroke('darkgray');
+        arc(xOrigin + permCircleCentre * xStep, yOrigin,
+          2 * permCircleRadius * xStep, 2 * permCircleRadius * yStep, PI, 0, OPEN);
+        stroke('dodgerblue');
+        arc(xOrigin + permCircleCentre * xStep, yOrigin,
+          2 * permCircleRadius * xStep, 2 * permCircleRadius * yStep,
+          PI + permAlpha, PI + permBeta, OPEN);
+      } else {
+        stroke('darkgray');
+        arc(xOrigin + permCircleCentre * xStep, yOrigin,
+          2 * permCircleRadius * xStep, 2 * permCircleRadius * yStep, PI, 0, OPEN);
+        stroke('dodgerblue');
+        arc(xOrigin + permCircleCentre * xStep, yOrigin,
+          2 * permCircleRadius * xStep, 2 * permCircleRadius * yStep,
+          PI + permBeta, PI + permAlpha, OPEN);
+      }
+      pop();
+
+      // drawing the points in the half-plane
+      push();
+      if (permX[index] && permY[index]) {
+        strokeWeight(2);
+        fill('white');
+        circle(xOrigin + permX[index] * xStep, yOrigin - permY[index] * yStep, 25);
+        fill('black');
+        textFont('Helvetica');
+        textSize(20);
+        text(vertexLabels[index], xOrigin + permX[index] * xStep, yOrigin - permY[index] * yStep);
+      }
+      if (permX[index + 1] && permY[index + 1]) {
+        strokeWeight(2);
+        fill('white');
+        circle(xOrigin + permX[index + 1] * xStep, yOrigin - permY[index + 1] * yStep, 25);
+        fill('black');
+        textFont('Helvetica');
+        textSize(20);
+        text(vertexLabels[index + 1], xOrigin + permX[index + 1] * xStep, yOrigin - permY[index + 1] * yStep);
+      }
+      pop();
+  }
+
+  // naming the inputted points permanently
+  push();
+  for (let index = 0; index <= permX.length; index++) {
+    fill('black');
+    textFont('Helvetica');
+    textSize(20);
+    if (permX[index] && permY[index]) {
+      text(vertexLabels[index] + ' = ' + permX[index] + ' + ' + permY[index] + 'i',
+            9 * windowWidth / 10, (index + 1.5) * windowHeight / 30 + 2);
+    }
+    if (permX[index + 1] && permY[index + 1]) {
+      text(vertexLabels[index + 1] + ' = ' + permX[index + 1] + ' + ' + permY[index + 1] + 'i',
+            9 * windowWidth / 10, (index + 1 + 1.5) * windowHeight / 30 + 2);
+    }
+  }
+  pop();
 }
